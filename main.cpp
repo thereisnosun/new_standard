bc5487d95d16d20c1835b7d401d0be0332b553ec
Teodor Moroz <teomoroz@gmail.com>
2019-03-15 08:22:50 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <variant>
+#include <filesystem>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Experiments with C++17\n";
+    std::variant<int, float> var;
+
+
+    return 0;
+}
